f3a4a3294571cf8294779b278be938d3d37944d6
imam9 <imamzunaidi4@gmail.com>
2023-08-08 23:12:05 +0700

    update fitur wishlist

--- lib/pages/home/wishlist_page.dart
+++ lib/pages/home/wishlist_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market/pages/widgets/wishlist_card.dart';
 import 'package:market/theme.dart';
 
 class WishlistPage extends StatelessWidget {
@@ -6,11 +7,106 @@ class WishlistPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Wishlist Page',
-         style: primaryTextStyle,
-      ),
+
+    Widget header(){
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Favorite Shoes',
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget emptyWishlist(){
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/image_wishlist.png',
+                width: 74,
+              ),
+              SizedBox(
+                height: 23,
+              ),
+              Text(
+                'You dont have dream shoes',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Le\'ts find your favorite shoes',
+                style: secondaryTextStyle,
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+              Container(
+                height: 44,
+                child: TextButton(
+                  onPressed: (){}, 
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 24
+                    ),
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)
+                    )
+                  ),
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  )
+                ),
+              ),
+            ],
+          ),
+
+        )
+      );
+    }
+
+    Widget content(){
+      return Expanded(
+        child: Container(
+          color: backgroundColor3,
+          child: ListView(
+            padding: EdgeInsets.symmetric(
+              horizontal: defaultMargin,
+            ),
+            children: [
+              WishlistCard(),
+              WishlistCard(),
+              WishlistCard(),
+            ],
+          ),
+        ),
+      );
+    }
+    return Column(
+      children: [
+        header(),
+        content(),
+        // emptyWishlist(),
+      ],
     );
   }
 }
